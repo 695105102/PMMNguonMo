21fd55f7ab42b20ea597c11c4185e6a3a6b530f7
695105102 <toantin.sogiaoduclamdong@gmail.com>
2023-03-21 20:24:35 +0700

    Master: sap xep va tim kiem

--- data/Bai_tap/Thuc hanh 3/function.cpp
+++ data/Bai_tap/Thuc hanh 3/function.cpp
@@ -3,6 +3,7 @@ int find_min(int *arr, int length);
 {
  // code
 }
+<<<<<<< HEAD
 int find_max(int *arr, int length)
 {
  // code
@@ -10,4 +11,9 @@ int find_max(int *arr, int length)
 int SORT(int *arr, int length)
 {
  // SORT cua nhanh feature
+=======
+void sort(int* arr, int length)
+{
+ // code: sort cua nhanh master
+>>>>>>> 4c1e720 (Master: sap xep va tim kiem)
 }
--- data/Bai_tap/Thuc hanh 3/function.h
+++ data/Bai_tap/Thuc hanh 3/function.h
@@ -1,5 +1,10 @@
 #include <iostream>
 using namespace std;
+<<<<<<< HEAD
 int find_min(int *arr, int length); 
 int find_max(int *arr, int length);
 int SORT(int *arr, int length);
+=======
+int search(int *arr, int length, int value);
+void sort(int* arr, int length);
+>>>>>>> 4c1e720 (Master: sap xep va tim kiem)
